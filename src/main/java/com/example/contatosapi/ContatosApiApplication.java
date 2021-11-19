0ae039ff641bc2034190b27363d8316c2a18af8b
isabellyoliveira1502 <isabellyoliveira1502@gmail.com>
2021-11-19 16:25:44 -0300

    commit inicial

---
+++ src/main/java/com/example/contatosapi/ContatosApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.contatosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContatosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContatosApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/contatosapi/model/AddEditEmpresaDto.java
@@ -0,0 +1,84 @@
+package com.example.contatosapi.model;
+
+import java.util.Date;
+
+public class AddEditEmpresaDto {
+private int tipo;
+private String nome;
+private String telefone;
+private String endereco;
+private String enderecoLogradouro;
+private String enderecoNumero;
+private String enderecoBairro;
+private String enderecoCidade;
+private String enderecoUf;
+private String enderecoCep;
+private Date createdAt;
+public int getTipo() {
+	return tipo;
+}
+public void setTipo(int tipo) {
+	this.tipo = tipo;
+}
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public String getTelefone() {
+	return telefone;
+}
+public void setTelefone(String telefone) {
+	this.telefone = telefone;
+}
+public String getEndereco() {
+	return endereco;
+}
+public void setEndereco(String endereco) {
+	this.endereco = endereco;
+}
+public String getEnderecoLogradouro() {
+	return enderecoLogradouro;
+}
+public void setEnderecoLogradouro(String enderecoLogradouro) {
+	this.enderecoLogradouro = enderecoLogradouro;
+}
+public String getEnderecoNumero() {
+	return enderecoNumero;
+}
+public void setEnderecoNumero(String enderecoNumero) {
+	this.enderecoNumero = enderecoNumero;
+}
+public String getEnderecoBairro() {
+	return enderecoBairro;
+}
+public void setEnderecoBairro(String enderecoBairro) {
+	this.enderecoBairro = enderecoBairro;
+}
+public String getEnderecoCidade() {
+	return enderecoCidade;
+}
+public void setEnderecoCidade(String enderecoCidade) {
+	this.enderecoCidade = enderecoCidade;
+}
+public String getEnderecoUf() {
+	return enderecoUf;
+}
+public void setEnderecoUf(String enderecoUf) {
+	this.enderecoUf = enderecoUf;
+}
+public String getEnderecoCep() {
+	return enderecoCep;
+}
+public void setEnderecoCep(String enderecoCep) {
+	this.enderecoCep = enderecoCep;
+}
+public Date getCreatedAt() {
+	return createdAt;
+}
+public void setCreatedAt(Date createdAt) {
+	this.createdAt = createdAt;
+}
+
+}
